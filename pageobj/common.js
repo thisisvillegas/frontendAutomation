96d1bd10d872fd5b86047ff85371d27a7e57abb7
thisisvillegas <thisisvillegas@gmail.com>
2020-07-16 12:21:16 -0500

    formatted

--- pageobj/common.js
+++ pageobj/common.js
@@ -2,8 +2,8 @@ const EC = protractor.ExpectedConditions;
 const { Logger } = require('../config/Logger');
 
 class Common {
-	constructor(browser) {
-		this.browser = browser;
+	constructor() {
+
         this.usernameField = element(by.id('id'));
 		this.passwordField = element(by.id('pw'));
         this.logInButton = element(by.className('login-text'));
@@ -67,43 +67,43 @@ class Common {
 		await el.click();
     }
 
-	async goToUrlAndWait(url, wait_url) {
-		await this.goToUrl(url);
-		await browser.wait(EC.urlContains(wait_url, 60000));
-	}
+	// async goToUrlAndWait(url, wait_url) {
+	// 	await this.goToUrl(url);
+	// 	await browser.wait(EC.urlContains(wait_url, 60000));
+	// }
 
-	async clickButtonWithText(name) {
-		Logger.info('Clicking button text ' + name);
-		const el = element(by.buttonText(name));
-		browser.wait(EC.elementToBeClickable(el));
-		await el.click();
-	}
+	// async clickButtonWithText(name) {
+	// 	Logger.info('Clicking button text ' + name);
+	// 	const el = element(by.buttonText(name));
+	// 	browser.wait(EC.elementToBeClickable(el));
+	// 	await el.click();
+	// }
 
-	async clickElementWithText(text) {
-		const el = element(by.xpath(`//div[contains(text(), "${text}")]`));
-		browser.wait(EC.elementToBeClickable(el));
-		await el.click();
-	}
+	// async clickElementWithText(text) {
+	// 	const el = element(by.xpath(`//div[contains(text(), "${text}")]`));
+	// 	browser.wait(EC.elementToBeClickable(el));
+	// 	await el.click();
+	// }
 
-	async clickLinkText(text) {
-		const el = element(by.linkText(text));
-		browser.wait(EC.elementToBeClickable(el));
-		await el.click();
-	}
+	// async clickLinkText(text) {
+	// 	const el = element(by.linkText(text));
+	// 	browser.wait(EC.elementToBeClickable(el));
+	// 	await el.click();
+	// }
 
-	async clickDropdownText(text) {
-		Logger.info('Clicking dropdown text ' + text);
-		const el = element(by.xpath(`//div[contains(@class, 'echo-component-EchoDropDown')]//li//*[text()='${text}']`));
-		browser.wait(EC.elementToBeClickable(el));
-		await el.click();
-	}
+	// async clickDropdownText(text) {
+	// 	Logger.info('Clicking dropdown text ' + text);
+	// 	const el = element(by.xpath(`//div[contains(@class, 'echo-component-EchoDropDown')]//li//*[text()='${text}']`));
+	// 	browser.wait(EC.elementToBeClickable(el));
+	// 	await el.click();
+	// }
 
 
-	waitForButtonTextToBePresent(text) {
-		Logger.info('Waiting for button text ' + text);
-		const el = element(by.buttonText(text));
-		return el.isPresent();
-	}
+	// waitForButtonTextToBePresent(text) {
+	// 	Logger.info('Waiting for button text ' + text);
+	// 	const el = element(by.buttonText(text));
+	// 	return el.isPresent();
+	// }
 
 	
 }
--- spec.js
+++ spec.js
@@ -1,79 +1,77 @@
 // const EC = protractor.ExpectedConditions;
-const Common = require("./pageobj/common");
-const { Logger } = require("./config/Logger");
+const Common = require('./pageobj/common');
+const { Logger } = require('./config/Logger');
 
-describe("Protractor Demo App\n", () => {
-  beforeAll(async () => {
-    common = new Common();
-    browser.waitForAngularEnabled(false);
-    Logger.info("fetching site");
-    browser.get("https://ca-test.planwithvoyant.com/advisergo");
-    await browser.sleep(2000);
+describe('Protractor Demo App\n', () => {
+	beforeAll(async () => {
+		common = new Common();
+		browser.waitForAngularEnabled(false);
+		Logger.info('fetching site');
+		browser.get('https://ca-test.planwithvoyant.com/advisergo');
+		await browser.sleep(2000);
 
-    Logger.info("entering Credentials");
-    common.usernameField.sendKeys("voyant-exercise");
-    common.passwordField.sendKeys("password100");
-    common.logInButton.click();
-  });
+		Logger.info('entering Credentials');
+		common.usernameField.sendKeys('voyant-exercise');
+		common.passwordField.sendKeys('password100');
+		common.logInButton.click();
+	});
 
-  it("Adding a new client", async () => {
-    common.clickAddButton();
-    await browser.sleep(1000);
+	it('Adding a new client', async () => {
+		common.clickAddButton();
+		await browser.sleep(1000);
 
-    Logger.info("entering New Client Data");
-    element(by.id("newInputFirst")).sendKeys("Kai");
-    element(by.id("newInputLast")).sendKeys("Ryssdal");
-    element(by.id("newPersonInputBirthday")).sendKeys("1967");
+		Logger.info('entering New Client Data');
+		element(by.id('newInputFirst')).sendKeys('Kai');
+		element(by.id('newInputLast')).sendKeys('Ryssdal');
+		element(by.id('newPersonInputBirthday')).sendKeys('1967');
 
-    common.clickSaveButton();
-    await browser.sleep(3000);
-    // expect(browser.getURL())
+		common.clickSaveButton();
+		await browser.sleep(3000);
+		// expect(browser.getURL())
+	});
 
-  });
+	it('Should add Pre-retirement Amount', async () => {
+		await common.clickAddButton();
+		await common.clickGoalsMenuButton();
+		await browser.sleep(1000);
+		await common.preretirementMenuButton.click();
+		await browser.sleep(1000);
 
- it("Should add Pre-retirement Amount", async () => {
-    await common.clickAddButton();
-    await common.clickGoalsMenuButton()
-    await browser.sleep(1000);
-    await common.preretirementMenuButton.click();
-    await browser.sleep(1000);
+		Logger.info('entering pre-retirement amount');
+		element(by.id('basicExpenseInputAmount')).sendKeys('1000');
 
-    Logger.info("entering pre-retirement amount");
-    element(by.id("basicExpenseInputAmount")).sendKeys("1000");
+		await common.clickSaveButton();
+	});
 
-    await common.clickSaveButton();
-  });
+	it('Should add new employment data', async () => {
+		await common.clickAddButton();
+		await common.clickIncomeMenuButton();
+		await browser.sleep(1000);
+		await common.employmentMenuButton.click();
+		await browser.sleep(1000);
 
-  it("Should add new employment data", async () => {
-    await common.clickAddButton();
-    await common.clickIncomeMenuButton()
-    await browser.sleep(1000);
-    await common.employmentMenuButton.click();
-    await browser.sleep(1000);
+		Logger.info('entering new employment data');
+		element(by.id('employmentInputName')).sendKeys('Voyant');
+		element(by.id('employmentInputSalary')).sendKeys('110000');
 
-    Logger.info("entering new employment data");
-    element(by.id("employmentInputName")).sendKeys("Voyant");
-    element(by.id("employmentInputSalary")).sendKeys("110000");
+		await common.clickSaveButton();
 
-    common.clickSaveButton();
+		//   element(by.linkText('Income')).click()
+		//   expect(element(by.linkText('Voyant')).toBeDefined())
+		//   expect(element(by.xpath('/html/body/div/div/div[2]/div[4]/div[2]/div[1]/div/div/div/div[2]/div/div[3]/div/div/div/div/div/div/table/tbody/tr/td[3]/span/span')).toEqual('$110,000'))
+	});
 
-    //   element(by.linkText('Income')).click()
-    //   expect(element(by.linkText('Voyant')).toBeDefined())
-    //   expect(element(by.xpath('/html/body/div/div/div[2]/div[4]/div[2]/div[1]/div/div/div/div[2]/div/div[3]/div/div/div/div/div/div/table/tbody/tr/td[3]/span/span')).toEqual('$110,000'))
-  });
+	it('Should add new insurance data', async () => {
+		await common.clickAddButton();
+		await common.clickInsuranceMenuButton();
+		await browser.sleep(1000);
+		await common.termlifeMenuButton.click();
+		await browser.sleep(1000);
 
-  it("Should add new insurance data", async () => {
-    await common.clickAddButton();
-    await common.clickInsuranceMenuButton()
-    await browser.sleep(1000);
-    await common.termlifeMenuButton.click();
-    await browser.sleep(1000);
+		Logger.info('entering new insurance data');
+		element(by.id('termLifeName')).sendKeys('Hi-Fidelity');
+		element(by.cssContainingText('option', 'Voyant')).click();
 
-    Logger.info("entering new insurance data");
-    element(by.id("termLifeName")).sendKeys("Hi-Fidelity");
-    element(by.cssContainingText("option", "Voyant")).click();
-
-    common.clickSaveButton();
-
-  });
+		await common.clickSaveButton();
+	});
 });
